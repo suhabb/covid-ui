fb3ddda728059d8c8f91ad661554046c64d4fb5d
suhailmir <suhabb@gmail.com>
2021-07-19 21:10:46 +0100

    vaccien area chart

--- src/d3_graph/vaccination/area_chart/vaccine-area-chart.js
+++ src/d3_graph/vaccination/area_chart/vaccine-area-chart.js
@@ -9,69 +9,78 @@ const AreaView = (props) => {
     if(countryData.data.length > 10){
      countryData.data =  countryData.data.slice(Math.max(countryData.data.length - 10, 0))
     }
+  
     countryData.data.forEach(item => {
         let vaccineData = {
             'daily_vaccinations': item.daily_vaccinations,
-            'date': item.date
+            'vaccine_date': item.date
         }
-        dataset.push(vaccineData)
+        dataset.push(vaccineData);
+    });
+    dataset.forEach(function (d) {
+        d.date = d3.timeParse("%Y-%m-%d")(d.vaccine_date);
     });
     console.log('Dataset', dataset);
-
     d3.select('.vaccine-areachart  > *').remove();
+
     const margin = { top: 20, right: 20, bottom: 130, left: 70 }
     const graphWidth = props.width - margin.left - margin.right;;
     const graphHeight = props.height - margin.top - margin.bottom;
 
-    const svg = d3.select('.vaccine-areachart ')
-        .append('svg')
-        .attr('width', graphWidth + margin.left + margin.right)
-        .attr('height', graphHeight + margin.top + margin.bottom)
-        .append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`);
+    let svg = d3.select(".vaccine-areachart")
+        .append("svg")
+        .attr("width", graphWidth + margin.left + margin.right)
+        .attr("height", graphHeight + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-  // Add X axis --> it is a date format
+    dataset.forEach(function (d) {
+        d.vaccine_date = d3.timeParse("%Y-%m-%d")(d.vaccine_date);
+    });
+
+    // Add X axis --> it is a date format
     let x = d3.scaleTime()
-        .domain(d3.extent(dataset,  (d) => new Date(d.date)))
+        .domain(d3.extent(dataset, function (d) { return d.vaccine_date; }))
         .range([0, graphWidth]);
     svg.append("g")
-        .attr("transform", `translate(0,${graphHeight})`)
+        .attr("transform", "translate(0," + graphHeight + ")")
         .call(d3.axisBottom(x));
 
     // Add Y axis
     var y = d3.scaleLinear()
-        .domain([0, d3.max(dataset, d => d.daily_vaccinations)])
+        .domain([0, d3.max(dataset, function (d) { return d.daily_vaccinations; })])
         .range([graphHeight, 0]);
     svg.append("g")
-        .call(d3.axisLeft(y));
+        .call(d3.axisLeft(y)
+        .tickFormat(d => {
+            return numFormatter(d);
+        }))
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 0 - margin.left)
+        .attr('x', 0 - (graphHeight / 2))
+        .attr('dy', '1em')
+        .style('text-anchor', 'middle')
+        .style('fill', '#141414' )
+        .text('Daily Vaccinations');
 
-      svg.append('g')
-          .call(d3.axisLeft(y)
-              .tickFormat(d => {
-                  return numFormatter(d.daily_vaccinations);
-              }))
-          .append('text')
-          .attr('transform', 'rotate(-90)')
-          .attr('y', 5 - margin.left)
-          .attr('x', 0 - (graphHeight / 2))
-          .attr('dy', '1em')
-          .style('text-anchor', 'middle')
-          .style('fill', '#141414')
-          .text('Daily Vaccination');
-  
-  
-      // Add the line
-      svg.append('path')
-          .datum(dataset)
-          .attr('fill', '#cce5df')
-          .attr('stroke', 'steelblue')
-          .attr('stroke-width', 1.5)
-          .attr('d', d3.area()
-              .x(function (d) { return x(new Date(d.date)) })
-              .y0(y(0))
-              .y1(function (d) { return y(d.daily_vaccinations) })
-          )
+    // Add the line
+    let curve = d3.curveLinear;
+
+    svg.append("path")
+        .datum(dataset)
+        .attr('fill', '#cce5df')
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.area()
+            .curve(curve)
+            .x(d => x(d.vaccine_date))
+            .y0(y(0))
+            .y1(d => y(d.daily_vaccinations) )
+        );
 }
+
 
 const numFormatter = (num) => {
     if (num > 999 && num < 1000000) {
